ddcfe0d706195611d1b0a72491ad0b73309c275b
mafuqiang <mafuqiang@outlook.com>
2018-08-27 17:03:31 +0800

    random

--- basic/src/main/RandomDemo.java
+++ basic/src/main/RandomDemo.java
@@ -1,12 +1,38 @@
 package main;
 
+import org.junit.Test;
+
 import java.util.Random;
+import java.util.Scanner;
 
 public class RandomDemo {
-    public static void main(String[] args) {
+    @Test
+    public void demo() {
         Random random = new Random();
 
         int i = random.nextInt(100); // [0, bound) 之间值
         System.out.println(i);
+    }
+
+    /**
+     * 猜数字
+     * */
+    public static void main(String[] args) throws InterruptedException {
+        Random r = new Random();
+        Scanner s = new Scanner(System.in);
+
+        int guessValue = r.nextInt(10);
+
+        while (true) {
+            int is = s.nextInt();
+            if (is == guessValue) {
+                System.out.println("--------------- ========= ----------------");
+                Thread.sleep(10000);
+                System.exit(0);
+            }
+            String msg = is > guessValue ? "大了" : "小了";
+            System.out.println(msg);
+        }
+
     }
 }
