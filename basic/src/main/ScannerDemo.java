2b461ed819c85b2c36a14f923624cfa87085f6bb
mafuqiang <mafuqiang@outlook.com>
2018-08-28 17:34:03 +0800

    class path

--- basic/src/main/ScannerDemo.java
+++ basic/src/main/ScannerDemo.java
@@ -14,4 +14,19 @@ public class ScannerDemo {
         Long res = Long.max(l, i);
         System.out.println(res);
     }
+
+    void diff() {
+        System.out.println("---->Test1:");
+        Scanner scanner = new Scanner(System.in);
+        String nextStr = scanner.next();
+        System.out.println("scanner.next()得到：" + nextStr);
+        String nextlineStr = scanner.nextLine();
+        System.out.println("scanner.nextLine()得到：" + nextlineStr);
+
+        System.out.println("\n---->Test2:");
+        String nextlineStr2 = scanner.nextLine();
+        System.out.println("scanner.nextLine()得到：" + nextlineStr2);
+        String nextStr2 = scanner.next();
+        System.out.println("scanner.next()得到：" + nextStr2);
+    }
 }
