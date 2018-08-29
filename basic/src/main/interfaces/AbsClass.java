924b53a4bcac1c8de197c1f15ae5e013a3f34ee5
mafuqiang <mafuqiang@outlook.com>
2018-08-29 16:07:32 +0800

    abstract interface 注释修饰

---
+++ basic/src/main/interfaces/AbsClass.java
@@ -0,0 +1,11 @@
+package main.interfaces;
+
+public abstract class AbsClass {
+    /* # private*/ protected abstract void run(); // abstract 方法不可以 [private] 否则不能集成覆盖->无用, 其他按需正常
+    // abstract void run(); // 包访问权, 类的 public 无用了
+    public abstract void run(int i); // abstract 方法不可以 [private] 否则不能集成无用
+}
+
+abstract class AbsA {
+    abstract void run();
+}
--- basic/src/main/interfaces/Interfaces.java
+++ basic/src/main/interfaces/Interfaces.java
@@ -8,9 +8,40 @@ package main.interfaces;
 public interface Interfaces {
     public static final int i = 0; // 常量 [public static final] 修饰
 
-    public void runMethod(); // 借口方法定义 [public]
+    public abstract void runMethod(); // 借口方法定义 [public abstract]
     public static int run() { // 静态方法实现 [public]
         System.out.println(999);
         return 999;
+    }
+}
+
+interface A {
+    A run();
+}
+
+interface B extends A {
+    B run();
+
+    B say();
+}
+
+class Crun implements B {
+
+    @Override
+    public B run() {
+        System.out.println("run");
+        return this;
+    }
+
+    @Override
+    public B say() {
+        System.out.println("say");
+        return this;
+    }
+
+    public static void main(String[] args) {
+        Crun crun = (Crun) new Crun().say();
+        new Crun().say().run();
+        System.out.println();
     }
 }
