6efa902ef1e983008960f7bb81b161d28f0e3db0
yy <mafuqiang@outlook.com>
2018-09-01 13:16:54 +0800

    thread

---
+++ basic/src/main/threads/MyThread.java
@@ -0,0 +1,8 @@
+package main.threads;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("running...");
+    }
+}
---
+++ basic/src/main/threads/RunnableThread.java
@@ -0,0 +1,8 @@
+package main.threads;
+
+public class RunnableThread implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(getClass().getName());
+    }
+}
