01cf52c8b0e9a5af6aba40cbc22313b6126f0cfa
mafuqiang <mafuqiang@outlook.com>
2018-09-04 14:28:05 +0800

    http socket

--- basic/src/main/net/blogs/HttpBrowser.java
+++ basic/src/main/net/blogs/HttpBrowser.java
@@ -1,16 +1,25 @@
 package main.net.blogs;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpBrowser {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(8888);
+        while (true) {
+            loopSocket(ss);
+        }
+    }
 
+    private static void loopSocket(ServerSocket ss) throws IOException {
         Socket s = ss.accept();
+
+        // read
+        InputStream in = s.getInputStream();
+        // BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        System.out.println(s);
 
         // out
         PrintWriter writer = new PrintWriter(s.getOutputStream(), true);
