754fd82ba893783f712ad3e4a2c4cea1d9b634e2
mafuqiang <mafuqiang@outlook.com>
2018-09-18 11:55:59 +0800

    multipart/formData 解析

---
+++ servlet/src/main/java/net/htmlonlnie/runs/ReqParas.java
@@ -0,0 +1,46 @@
+package net.htmlonlnie.runs;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/p")
+public class ReqParas extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("ok...");
+
+        // post Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryOirybHQait1GpqGA
+        // file-upload
+        FileItemFactory factory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        List<FileItem> items = null;
+        try {
+            items = upload.parseRequest(request);
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        }
+        for(FileItem item: items)
+        {
+            String fieldName = item.getFieldName();
+            String fieldValue = item.getString();
+        }
+
+        System.out.println(request);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
