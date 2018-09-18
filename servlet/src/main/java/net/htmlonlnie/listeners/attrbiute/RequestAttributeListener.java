28745fb1f11d5bcae49f32ebc0f640173002e7b2
mafuqiang <mafuqiang@outlook.com>
2018-09-18 17:10:24 +0800

    update listener

--- servlet/src/main/java/net/htmlonlnie/listeners/attrbiute/RequestAttributeListener.java
+++ servlet/src/main/java/net/htmlonlnie/listeners/attrbiute/RequestAttributeListener.java
@@ -2,8 +2,6 @@ package net.htmlonlnie.listeners.attrbiute;
 
 import javax.servlet.ServletRequestAttributeEvent;
 import javax.servlet.ServletRequestAttributeListener;
-import javax.servlet.annotation.WebListener;
-import javax.servlet.annotation.WebServlet;
 
 // @WebListener
 public class RequestAttributeListener implements ServletRequestAttributeListener {
