1b4804c0dfd5581fdcdc7ee33aef8c21f6806df2
mafuqiang <mafuqiang@outlook.com>
2018-09-19 18:50:12 +0800

    delete autoLogin

--- autoLoginTest/src/main/java/net/htmlonline/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package net.htmlonline.dao;
-
-import net.htmlonline.domain.UserBean;
-
-import java.sql.SQLException;
-
-public interface UserDao {
-    UserBean login(UserBean userBean) throws SQLException;
-}
