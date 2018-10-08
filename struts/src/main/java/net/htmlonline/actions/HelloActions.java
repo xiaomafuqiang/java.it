4a5df375c756fb9e252c96a6dfeacf3a916a1c15
mafuqiang <mafuqiang@outlook.com>
2018-10-08 12:14:23 +0800

    STRUTS INIT

---
+++ struts/src/main/java/net/htmlonline/actions/HelloActions.java
@@ -0,0 +1,10 @@
+package net.htmlonline.actions;
+
+public class HelloActions {
+
+    public String execute() {
+
+        System.out.println("hello actions... ");
+        return "success";
+    }
+}
