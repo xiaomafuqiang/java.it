270deaa7cea5c95a5dd73a595b405d04e8c51f3a
mafuqiang <mafuqiang@outlook.com>
2018-10-09 15:57:11 +0800

    STRUTS test

---
+++ struts/src/main/java/net/htmlonline/test/Play.java
@@ -0,0 +1,14 @@
+package net.htmlonline.test;
+
+import ognl.OgnlContext;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class Play {
+
+    @Test
+    public void run() {
+        System.out.println(888);
+    }
+}
