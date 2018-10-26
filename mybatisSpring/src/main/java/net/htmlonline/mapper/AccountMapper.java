4ca71fb8effed009adf30e6c9f4b22be4627e39c
mafuqiang <mafuqiang@outlook.com>
2018-10-26 11:38:42 +0800

    mybatis spring mapper factory 完全配置方式

---
+++ mybatisSpring/src/main/java/net/htmlonline/mapper/AccountMapper.java
@@ -0,0 +1,7 @@
+package net.htmlonline.mapper;
+
+import net.htmlonline.domain.Account;
+
+public interface AccountMapper {
+    Account getAccountById(Integer id);
+}
---
+++ mybatisSpring/src/main/java/net/htmlonline/mapper/AccountMapperTest.java
@@ -0,0 +1,19 @@
+package net.htmlonline.mapper;
+
+import net.htmlonline.domain.Account;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AccountMapperTest {
+
+    @Test
+    public void run() {
+        System.out.println(99);
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        // AccountMapper accountMapper = context.getBean("accountMapper", AccountMapper.class); // 方式 一
+         AccountMapper accountMapper = context.getBean(AccountMapper.class); // 方式二
+        Account accoutById = accountMapper.getAccountById(14);
+        System.out.println(accoutById);
+    }
+}
